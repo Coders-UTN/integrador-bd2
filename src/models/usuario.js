d50d6282e606025c64d0301e049e8a55fd1efb50
rodry <rodrymza@gmail.com>
2025-10-30 10:56:35 -0300

    creado modelo de usuario

---
+++ src/models/usuario.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema(
+  {
+    apellido: { type: String, required: [true, "El apellido es obligatorio"] },
+    nombre: { type: String, required: [true, "El nombre es obligatorio"] },
+    dni: String,
+    email: {
+      type: String,
+      required: [true, "El email es obligatorio"],
+      unique: true,
+      trim: true,
+    },
+    direccion: {
+      type: String,
+      required: [true, "La direccion es obligatoria"],
+    },
+    telefono: String,
+    rol: {
+      type: String,
+      enum: {
+        values: ["cliente", "admin"],
+        message: "{VALUE} no es un rol valido",
+      },
+      default: "cliente",
+    },
+    contrasenia: String,
+  },
+  { timestamps: true }
+);
+
+export const Usuario = mongoose.model("Usuario", usuarioSchema);
