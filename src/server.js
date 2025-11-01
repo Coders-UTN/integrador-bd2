8d49d4659533ac639ed2d61d152188935851e152
rodrymza <164051945+Rodrymza@users.noreply.github.com>
2025-11-01 18:46:16 -0300

    Refactorizar manejador de errores, nuevas rutas

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config'; 
+import { categoriaRoutes } from './routes/categoriaRoutes.js';
+import { productoRoutes } from './routes/productoRoutes.js';
 
-import { rutasUsuario } from './routes/usuarioRoutes.js';
+//import { rutasUsuario } from './routes/usuarioRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +21,8 @@ mongoose.connect(MONGO_URI)
     .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 app.use('/api/usuarios', rutasUsuario);
+app.use("/api/categorias", categoriaRoutes);
+app.use("/api/productos", productoRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err.stack); 
@@ -26,8 +30,13 @@ app.use((err, req, res, next) => {
     if (err.name === 'ValidationError') {
         return res.status(400).json({ success: false, error: err.message });
     }
-    if (err.code === 11000) { 
-        return res.status(400).json({ success: false, error: 'El email ya está en uso.' });
+if (err.code === 11000) { 
+
+        const field = Object.keys(err.keyValue)[0];
+        const value = err.keyValue[field];
+        const message = `El campo '${field}' con el valor '${value}' ya existe.`;
+
+        return res.status(400).json({ success: false, error: message });
     }
 
     res.status(500).json({ 
