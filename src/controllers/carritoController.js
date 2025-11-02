fae008a80eff8ab005b788604f81c26ef3b7e190
rodry <rodrymza@gmail.com>
2025-11-02 11:39:17 -0300

    funciones para buscar carrito y calcular total

---
+++ src/controllers/carritoController.js
@@ -0,0 +1,66 @@
+import { Carrito } from "../models/carrito.js";
+
+export const buscarCarritoUsuario = async (req, res, next) => {
+  try {
+    const { usuarioId } = req.params;
+
+    const carrito = await Carrito.findOne({ usuario: usuarioId }).populate(
+      "items.producto",
+      "nombre precio stock"
+    );
+
+    if (!carrito) {
+      return res.status(200).json({
+        items: [],
+        usuario: usuarioId,
+      });
+    }
+
+    return res.status(200).json(carrito);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const calcularTotalCarrito = async (req, res, next) => {
+  try {
+    const { usuarioId } = req.params;
+    const carrito = await Carrito.findOne({ usuario: usuarioId }).populate(
+      "items.producto",
+      "nombre precio stock"
+    );
+
+    if (!carrito) {
+      return res.status(200).json({
+        items: [],
+        totalGeneral: 0,
+      });
+    }
+
+    let totalGeneral = 0;
+    const itemsConSubtotal = carrito.items.map(item => {
+      // verificacion en caso de producto borrado
+      if (item.producto) {
+        const subtotalItem = item.producto.precio * item.cantidad;
+        totalGeneral += subtotalItem;
+
+        return {
+          productoId: item.producto._id,
+          nombre: item.producto.nombre,
+          precioUnitario: item.producto.precio,
+          cantidad: item.cantidad,
+          subtotal: subtotalItem,
+					stockDisponible : item.producto.stock
+        };
+      }
+      return null; // Si el producto fue borrado, lo marcamos como nulo
+    }).filter(item => item !== null); // Filtramos los items nulos (productos borrados)
+
+    return res.status(200).json({
+      items: itemsConSubtotal,
+      totalGeneral: totalGeneral,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
