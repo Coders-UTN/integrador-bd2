17a1d63edc24c50c69ba12da0faf2dc4c32f8bbd
rodry <rodrymza@gmail.com>
2025-11-03 09:01:13 -0300

    añadidas funcioens para crear, actualizar y calcular total y agrupar pedidos por estado

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,29 +1,73 @@
 import { Pedido } from "../models/pedido";
 
+export const crearPedido = async (req, res, next) => {
+  try {
+    const { usuario, items, total, metodoPago } = req.body;
+    const nuevoPedido = new Pedido(usuario, items, total, metodoPago);
+
+    const pedidoGuardado = await nuevoPedido.save();
+
+    return res.status(201).json(pedidoGuardado);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const mostrarTodosPedidos = async (req, res, next) => {
   try {
     const pedidos = await Pedido.find()
       .populate("itemPedido", "nombre  precioUnitario cantidad")
       .populate("usuario", "apellido nombre dni email");
 
-      return res.status(200).json(pedidos);
+    return res.status(200).json(pedidos);
   } catch (error) {
     next(error);
-  } 
+  }
 };
 
-//pedidos por estado
+export const totalPedidosEstado = async (req, res, next) => {
+  try {
+    const pedidosEstado = await Pedido.aggregate([
+      {
+        $group: {
+          _id: "$estado",
+          totalPedidos: { $sum: 1 },
+        },
+      },
+    ]);
 
+    return res.status(200).json(pedidosEstado);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const buscarPedidoUsuario = async (req, res, next) => {
   try {
-    const { idUsuario } = req.params;
-    const pedidosUsuario = await Pedido.find({usuario : idUsuario})
+    const { usuarioId } = req.params;
+    const pedidosUsuario = await Pedido.find({ usuario: usuarioId })
       .populate("itemPedido", "nombre  precioUnitario cantidad")
       .populate("usuario", "apellido nombre");
 
-      return res.status(200).json(pedidosUsuario);
+    return res.status(200).json(pedidosUsuario);
   } catch (error) {
     next(error);
-  }  
+  }
+};
+
+export const actualizarEstado = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { nuevoEstado } = req.body;
+
+    const pedidoActualizado = await Pedido.findByIdAndUpdate(
+      id,
+      { estado: nuevoEstado },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(pedidoActualizado);
+  } catch (error) {
+    next(error);
+  }
 };
